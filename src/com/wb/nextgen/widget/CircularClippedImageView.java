8a8f19725df233d007fdacf18f36ec65c76e30ab
Grant Cheng <grant.cheng@warnerbros.com>
2016-03-04 12:36:01 -0800

    fixed selected state ring overlay

--- src/com/wb/nextgen/widget/CircularClippedImageView.java
+++ src/com/wb/nextgen/widget/CircularClippedImageView.java
@@ -12,6 +12,8 @@ import android.graphics.Rect;
 import android.graphics.RectF;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
 import android.os.Build;
 import android.util.AttributeSet;
 import android.widget.ImageView;
@@ -59,6 +61,13 @@ public class CircularClippedImageView extends ImageView{
         super.setImageBitmap(bm);
     }
 
+    public void setActivated(boolean bool){
+        this.getTag();
+        super.setActivated(bool);
+        dispatchDraw(new Canvas());
+    }
+
+
     private Bitmap circularClipBitmap(Bitmap bm){
         Bitmap output = Bitmap.createBitmap(bm.getWidth(),
                 bm.getHeight(), Bitmap.Config.ARGB_8888);
@@ -75,23 +84,57 @@ public class CircularClippedImageView extends ImageView{
         float centerX = getWidth() / 2;
         float centerY = getHeight() / 2;
         float radius = Math.min(centerX, centerY);
-        if (isSelected()){
-            float insideRaius = radius - getResources().getDimensionPixelSize(R.dimen.selected_thumbnail_border_size);
-            /*Paint selectedPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-            selectedPaint.setColor(getResources().getColor(android.R.color.white));
-            canvas.drawCircle(centerX, centerY, radius, selectedPaint);*/
-            canvas.drawCircle(centerX, centerY, insideRaius, paint);
+        float insideRaius = radius - getResources().getDimensionPixelSize(R.dimen.selected_thumbnail_border_size);
 
-        }else
-            canvas.drawCircle(centerX, centerY, radius, paint);
+        canvas.drawCircle(centerX, centerY, insideRaius, paint);
         paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
         canvas.drawBitmap(bm, rect, rect, paint);
+        if (isActivated()) {
+            //create another bitmap for selected overlay
+            Bitmap overlay = Bitmap.createBitmap(bm.getWidth(),
+                    bm.getHeight(), Bitmap.Config.ARGB_8888);
+            Canvas onCanvas = new Canvas(overlay);
 
-        /*Paint selectedPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        selectedPaint.setColor(getResources().getColor(android.R.color.white));
-        canvas.drawCircle(centerX, centerY, radius, selectedPaint);*/
+            // draw a circle on the overlay bitmap
+            Paint selectedPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+            selectedPaint.setColor(getResources().getColor(android.R.color.white));
+            onCanvas.drawCircle(centerX, centerY, radius, selectedPaint);
 
+            selectedPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+            selectedPaint.setColor(getResources().getColor(android.R.color.black));
+            onCanvas.drawCircle(centerX, centerY, insideRaius, selectedPaint);
+
+            // draw on top of normal state image
+            selectedPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.ADD));
+            canvas.drawBitmap(overlay, rect, rect, selectedPaint);
+        }
         return output;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        /*if (isActivated()) {
+
+
+            RectF outerRectangle = new RectF(0, 0, getWidth(), getHeight());
+
+            Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+            paint.setColor(getResources().getColor(android.R.color.white));
+            //paint.setAlpha(100);
+            RectF oval = new RectF(width/2 - radius, width/2 - radius, width/2 + radius, width/2 + radius);
+            canvas.drawArc(oval, 0, 360, false, paint1);
+            canvas.drawRect(outerRectangle, paint);
+
+            paint.setColor(Color.TRANSPARENT);
+            paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_OUT));
+            float centerX = getWidth() / 2;
+            float centerY = getHeight() / 2;
+            float radius = Math.min(centerX, centerY);//getResources().getDimensionPixelSize(R.dimen.next_gen_actor_thumbnail_cirle_mask_radius);
+            canvas.drawCircle(centerX, centerY, radius, paint);
+
+
+        }*/
     }
 
     @Override
