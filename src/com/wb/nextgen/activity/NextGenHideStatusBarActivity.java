06ef39245219395133b35b0ed6f523885e483fbf
Grant Cheng <grant.cheng@warnerbros.com>
2016-07-28 11:37:18 -0700

    cleanup

--- src/com/wb/nextgen/activity/NextGenHideStatusBarActivity.java
+++ src/com/wb/nextgen/activity/NextGenHideStatusBarActivity.java
@@ -14,40 +14,18 @@ import android.view.View;
 public class NextGenHideStatusBarActivity extends AppCompatActivity {
     private static int currentScreenOrientation = -1;
 
-    static int DESIRE_VISIBILITY = //View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
-            //View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+    static int DESIRE_VISIBILITY =
             View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
             View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
             View.SYSTEM_UI_FLAG_FULLSCREEN |
             View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
-    static long RESETUI_TIMER = 3000L;
 
     @Override
     protected void onStart() {
         super.onStart();
         currentScreenOrientation = getScreenOrientation();
         getWindow().getDecorView().setSystemUiVisibility(getWindow().getDecorView().getSystemUiVisibility() | DESIRE_VISIBILITY);
-        /*getWindow().getDecorView().setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
-            @Override
-            public void onSystemUiVisibilityChange(final int visibility) {
-                if ( (visibility & DESIRE_VISIBILITY) != DESIRE_VISIBILITY){
 
-                    hideBarsHandler.postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    getWindow().getDecorView().setSystemUiVisibility(visibility | DESIRE_VISIBILITY); // hide nav bar
-
-                                }
-                            });
-                        }
-                    }, RESETUI_TIMER);
-
-                }
-            }
-        });*/
 
     }
 
@@ -56,9 +34,6 @@ public class NextGenHideStatusBarActivity extends AppCompatActivity {
         if (hasFocus)
             getWindow().getDecorView().setSystemUiVisibility(getWindow().getDecorView().getSystemUiVisibility() | DESIRE_VISIBILITY);
     }
-
-    final Handler hideBarsHandler = new Handler();
-
 
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
