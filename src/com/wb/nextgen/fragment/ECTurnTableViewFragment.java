d3312d587dde462dd9eaaff37014c1dc0b4f86f0
Grant Cheng <grant.cheng@warnerbros.com>
2016-07-28 15:22:13 -0700

    Added swipe gesture & carousel  for Turntable gallery added loading progress bar for Turntable Gallery

--- src/com/wb/nextgen/fragment/ECTurnTableViewFragment.java
+++ src/com/wb/nextgen/fragment/ECTurnTableViewFragment.java
@@ -6,6 +6,9 @@ import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
@@ -55,11 +58,32 @@ public class ECTurnTableViewFragment extends AbstractECGalleryViewFragment{
         turnTableSeekBar = (SeekBar) view.findViewById(R.id.turn_table_seekbar);
         turnTableSeekBar.setOnSeekBarChangeListener(seekBarListener);
         loadingProgressBar = (ProgressBar)view.findViewById(R.id.loading_progress_bar);
+        if (loadingProgressBar != null)
+            loadingProgressBar.setMax(100);
 
         turnTableImageView = (ImageView) view.findViewById(R.id.turn_table_image_view);
         if (currentGallery != null){
             setCurrentGallery(currentGallery);
         }
+
+        turnTableImageView.setOnTouchListener(new OnSwipeTouchListener());
+    }
+
+    void nextPic(){
+        int currentIndex = turnTableSeekBar.getProgress();
+        if (currentIndex < 49)
+            turnTableSeekBar.setProgress(currentIndex + 1);
+        else{
+            turnTableSeekBar.setProgress(0);
+        }
+    }
+
+    void prevPic(){
+        int currentIndex = turnTableSeekBar.getProgress();
+        if (currentIndex > 0)
+            turnTableSeekBar.setProgress(currentIndex - 1);
+        else
+            turnTableSeekBar.setProgress(50);
     }
 
     SeekBar.OnSeekBarChangeListener seekBarListener = new SeekBar.OnSeekBarChangeListener() {
@@ -69,9 +93,6 @@ public class ECTurnTableViewFragment extends AbstractECGalleryViewFragment{
                 turnTableImageView.setImageBitmap(turntableBitmaps.get(progress));
 
             }
-                //turnTableImageView.setImageBitmap(turntableBitmaps.get(progress));
-            //Picasso.with(getActivity()).load(currentGallery.galleryImages.get(progress).fullImage.url).into(turnTableImageView);
-            //Glide.with(getActivity()).load(currentGallery.galleryImages.get(progress).fullImage.url).into(turnTableImageView);
 
         }
 
@@ -118,6 +139,7 @@ public class ECTurnTableViewFragment extends AbstractECGalleryViewFragment{
                             seekBarListener.onProgressChanged(turnTableSeekBar, 0, false);
                             turnTableSeekBar.setMax(turntableBitmaps.size());
                             loadingProgressBar.setVisibility(View.GONE);
+                            loadingProgressBar.setProgress(0);
 
                         }
                     });
@@ -137,7 +159,7 @@ public class ECTurnTableViewFragment extends AbstractECGalleryViewFragment{
 
 
 
-    public void getImages(ResultListener<List<Bitmap>> l){
+    public void getImages(final ResultListener<List<Bitmap>> l){
         Worker.execute(new Callable<List<Bitmap>>() {
             @Override
             public List<Bitmap> call() throws Exception {
@@ -145,6 +167,7 @@ public class ECTurnTableViewFragment extends AbstractECGalleryViewFragment{
                 double fractionNumber = currentGallery.galleryImages.size() > 50 ? (double)currentGallery.galleryImages.size() / 50.0 : currentGallery.galleryImages.size();
 
                 List<Bitmap> result = new ArrayList<Bitmap>();
+
                 for (int i = 0 ; i * fractionNumber < currentGallery.galleryImages.size(); i++ ) {
                     MovieMetaData.PictureItem item = currentGallery.galleryImages.get((int)(i * fractionNumber));
 
@@ -154,6 +177,17 @@ public class ECTurnTableViewFragment extends AbstractECGalleryViewFragment{
                             into(RESTRICTED_IMAGE_WIDTH, RESTRICTED_IMAGE_HEIGHT). // Width and height
                             get();
                     result.add(theBitmap);
+                    final int progress = i*2;
+
+                    if (getActivity() != null){
+                        getActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                if (loadingProgressBar != null)
+                                    loadingProgressBar.setProgress(progress);
+                            }
+                        });
+                    }
                 }
 
 
@@ -162,4 +196,61 @@ public class ECTurnTableViewFragment extends AbstractECGalleryViewFragment{
         }, l);
     }
 
+
+    public class OnSwipeTouchListener implements View.OnTouchListener {
+
+        @SuppressWarnings("deprecation")
+        private final GestureDetector gestureDetector = new GestureDetector(new GestureListener());
+
+        @Override
+        public boolean onTouch(final View v, final MotionEvent event) {
+            return gestureDetector.onTouchEvent(event);
+        }
+
+        private final class GestureListener extends GestureDetector.SimpleOnGestureListener {
+
+            private static final int SCROLL_THRESHOLD = 5;
+
+            @Override
+            public boolean onDown(MotionEvent e) {
+                return true;
+            }
+            @Override
+            public boolean onSingleTapConfirmed(MotionEvent e) {
+                onTouch(null, e);
+                return true;
+            }
+
+            @Override
+            public boolean onScroll(MotionEvent e1, MotionEvent e2,
+                                    float distanceX, float distanceY) {
+                NextGenLogger.d(F.TAG, "Moved: " + distanceX);
+
+                int imageWidth = turnTableImageView.getWidth();
+                int threshold = imageWidth / 200;
+
+                if (distanceX > threshold) { //next page
+                    nextPic();
+                } else if (distanceX < -threshold) { //previous page
+                    prevPic();
+                }
+
+                return false;
+            }
+
+            @Override
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                boolean result = false;
+                try {
+                    float diffY = e2.getY() - e1.getY();
+                    float diffX = e2.getX() - e1.getX();
+
+                } catch (Exception exception) {
+                    exception.printStackTrace();
+                }
+                return result;
+            }
+        }
+
+    }
 }
