2a75186b7e899d88a59b3a18fde36282dae2d1a7
Grant Cheng <grant.cheng@warnerbros.com>
2017-05-15 16:49:30 -0700

    ORS#192317: Throughout the main feature when a Scene Location box appears when a Did You Know box is already present, the screenshot of the Did You Know box will briefly appear on the Scene Location

--- src/com/wb/nextgenlibrary/fragment/IMEElementsGridFragment.java
+++ src/com/wb/nextgenlibrary/fragment/IMEElementsGridFragment.java
@@ -291,25 +291,33 @@ public class IMEElementsGridFragment extends AbstractGridViewFragment implements
         if (activeObj.imeObject instanceof MovieMetaData.IMEElement) {
             Object dataObj = ((MovieMetaData.IMEElement) activeObj.imeObject).imeObject;
             if (dataObj instanceof MovieMetaData.PresentationDataItem) {
+                Object tag = rowView.getTag(R.id.ime_title);
+                boolean isSameItem = tag != null && tag.equals(((MovieMetaData.PresentationDataItem) dataObj).getId());
 
                 rowView.setTag(R.id.ime_title, ((MovieMetaData.PresentationDataItem) dataObj).getId());
                 if (dataObj instanceof MovieMetaData.LocationItem ){
 
                     MovieMetaData.LocationItem locationItem = (MovieMetaData.LocationItem) dataObj;
                     if (locationItem != null && poster.getHeight() > 0 && poster.getWidth() > 0){
-                        String imageUrl = locationItem.getGoogleMapImageUrl(poster.getWidth() / 2, poster.getHeight() / 2);
-                        NextGenGlide.load(getActivity(), imageUrl).centerCrop().into(poster);
+                        if (!isSameItem) {
+                            poster.setImageDrawable(null);
+                            String imageUrl = locationItem.getGoogleMapImageUrl(poster.getWidth() / 2, poster.getHeight() / 2);
+                            NextGenGlide.load(getActivity(), imageUrl).centerCrop().into(poster);
+                        }
                     }
 
                 } else if (poster != null) {
                     String imageUrl = ((MovieMetaData.PresentationDataItem) dataObj).getPosterImgUrl();
-					if (!StringHelper.isEmpty(imageUrl)) {
-                        NextGenGlide.load(getActivity(), imageUrl).centerCrop()
-								.into(poster);
-						poster.setVisibility(View.VISIBLE);
-					} else {
-						poster.setVisibility(View.GONE);
-					}
+                    if (!isSameItem){
+                        poster.setImageDrawable(null);
+                        if (!StringHelper.isEmpty(imageUrl)) {
+                            NextGenGlide.load(getActivity(), imageUrl).centerCrop()
+                                    .into(poster);
+                            poster.setVisibility(View.VISIBLE);
+                        } else {
+                            poster.setVisibility(View.GONE);
+                        }
+                    }
                 }
 
                 ImageView playbutton = (ImageView)rowView.findViewById(R.id.ime_item_play_logo);
